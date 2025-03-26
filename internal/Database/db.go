f6b02a6b08a16afd81a9afcb3c5ff44537fb99c3
quangnhathung <20182019quangnhathungpanasonic@gmail.com>
2025-03-26 17:33:21 +0700

    cnt2db

--- internal/Database/db.go
+++ internal/Database/db.go
@@ -9,7 +9,6 @@ import (
 )
 func ConnectToDatabase() (*gorm.DB,error){
 
-	//dsn := "host=localhost user=postgres password=hung05112005 dbname=vocab_db port=5432 sslmode=disable TimeZone=Asia/Ho_Chi_Minh"
 	dsn:=sevices.String_connect()
 	// connect to database
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
